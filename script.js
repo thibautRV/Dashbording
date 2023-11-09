f376ec7b39f7bb87ec4d534aa3cbcaab22f9583b
thibautRV <125503955+thibautRV@users.noreply.github.com>
2023-11-09 16:31:32 +0100

    Update script.js

--- script.js
+++ script.js
@@ -190,3 +190,20 @@ function createPieChart(data, attribute) {
     },
   });
 }
+
+function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+function getRandomColors(count) {
+  const colors = [];
+  for (let i = 0; i < count; i++) {
+    colors.push(getRandomColor());
+  }
+  return colors;
+}
